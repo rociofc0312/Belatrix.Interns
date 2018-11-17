e8b19301d2843e4eab70d84d266e09bd0bff86ac
Rocío Fernández <rociofc0312@gmail.com>
2018-11-17 12:36:40 -0500

    Implementation of Repository Pattern with Generics

---
+++ Belatrix.Repository.SqlServer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Belatrix.Repository.SqlServer
+{
+    public class Class1
+    {
+    }
+}
---
+++ Belatrix.Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace Belatrix.Repository
+{
+    public interface IRepository<T> where T: class
+    {
+        //CRUD
+        int Add(T entity);
+        IEnumerable<T> GetList();
+        bool Update(T entity);
+        bool Delete(T entity);
+    }
+}
